922d6117cea0994cd67e5d351ffdb435b871b38f
blueplanet <erguolinge@gmail.com>
2016-10-16 07:37:57 +0900

    add author name / id

--- app/models/blorgh/article.rb
+++ app/models/blorgh/article.rb
@@ -1,5 +1,16 @@
 module Blorgh
   class Article < ApplicationRecord
     has_many :comments
+
+    attr_accessor :author_name
+    belongs_to :author, class_name: 'User', dependent: :destroy
+
+    before_save :set_author
+
+    private
+
+      def set_author
+        self.author = User.find_or_create_by(name: author_name)
+      end
   end
 end

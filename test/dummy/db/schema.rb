4a12aa2acec024769eeaf7a371beff6665169e4f
blueplanet <erguolinge@gmail.com>
2016-10-16 07:21:20 +0900

    Add comment

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161015220552) do
+ActiveRecord::Schema.define(version: 20161015221329) do
 
   create_table "blorgh_articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "blorgh_comments", force: :cascade do |t|
+    t.text     "text"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_blorgh_comments_on_article_id"
   end
 
 end

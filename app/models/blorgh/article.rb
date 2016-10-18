c4fb681dd1fed66051f43326abdbc491fac7f90c
blueplanet <erguolinge@gmail.com>
2016-10-18 10:11:54 +0900

    Change to class

--- app/models/blorgh/article.rb
+++ app/models/blorgh/article.rb
@@ -3,14 +3,14 @@ module Blorgh
     has_many :comments
 
     attr_accessor :author_name
-    belongs_to :author, class_name: Blorgh.author_class, dependent: :destroy
+    belongs_to :author, class_name: Blorgh.author_class.to_s, dependent: :destroy
 
     before_validation :set_author
 
     private
 
       def set_author
-        self.author = Blorgh.author_class.constantize.find_or_create_by(name: author_name)
+        self.author = Blorgh.author_class.find_or_create_by(name: author_name)
       end
   end
 end

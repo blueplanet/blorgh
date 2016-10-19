0a78a727f26881bf6f7d72b44f3f62bf4c344314
blueplanet <erguolinge@gmail.com>
2016-10-20 06:57:22 +0900

    change to concern

--- app/models/blorgh/article.rb
+++ app/models/blorgh/article.rb
@@ -1,20 +1,7 @@
 module Blorgh
   class Article < ApplicationRecord
+    include Blorgh::Concerns::Models::Article
+
     has_many :comments
-
-    attr_accessor :author_name
-    belongs_to :author, class_name: Blorgh.author_class.to_s, dependent: :destroy
-
-    before_validation :set_author
-
-    def summary
-      "#{title}"
-    end
-
-    private
-
-      def set_author
-        self.author = Blorgh.author_class.find_or_create_by(name: author_name)
-      end
   end
 end
---
+++ lib/concerns/models/article.rb
@@ -0,0 +1,20 @@
+module Blorgh::Concerns::Models::Article
+  extend ActiveSupport::Concern
+
+  included do
+    attr_accessor :author_name
+    belongs_to :author, class_name: 'User'
+
+    before_validation :set_author
+
+    private
+
+      def set_author
+        self.author = User.find_or_create_by(name: author_name)
+      end
+  end
+
+  def summary
+    title
+  end
+end
